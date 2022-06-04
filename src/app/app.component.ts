c0329b730cc2fa2a7fcc6439c25ea9625638c112
quyenld <ducquyenluong97@gmail.com>
2022-06-04 21:48:32 +0700

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'game';
+  constructor(private http: HttpClient) {
+    
+  }
+  
+  ngOnInit() {
+    this.http.post(`https://api.fpt.ai/hmi/tts/v5`, 'Đây là nơi trưng bày các bảo bối thần kì từ đời đầu đến đời mới nhất của thế kỉ 22, Sóc Út đố bạn biết?', {
+      headers: new HttpHeaders({
+        'api-key': 'FcgCeDqR6Xd5xJB0Ok7cKiV7fpP6YRxK',
+        // 'callback_url': 'https://jsonplaceholder.typicode.com/posts'
+        'format': 'mp3'
+      })
+    }).subscribe(res => {
+      console.log('res: ', res);
+      
+    })
+  }
 }
